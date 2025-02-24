0eb3bb848f0685ecf8ace6d7c5431bbe29d53cde
g7g7g8 <artemstanovov36@gmail.com>
2025-02-24 19:19:18 +0300

    Добавил пунктир

--- src/physics/theRocheLimit/logic/createDottedCircle.ts
+++ src/physics/theRocheLimit/logic/createDottedCircle.ts
@@ -1,8 +1,25 @@
 export const createDottedCircle = (ctx: CanvasRenderingContext2D | null | undefined, radius: number) => {
+    const LENGTH_ARC = 12
+    const LENGTH_SPACE = 12
+    const sumLength = LENGTH_ARC + LENGTH_SPACE
+    const countArc = Math.floor(radius * 2 * Math.PI / sumLength)
+
+    const dottedArray = []
+    for (let i = 0; i < countArc; i++) {
+        dottedArray.push({
+            angleStart: (2 * Math.PI / countArc) * i,
+            angleEnd: (2 * Math.PI / countArc) * (i + (LENGTH_ARC / sumLength))
+        })
+    }
+    
+    console.log(dottedArray)
+
     if (ctx) {
-        ctx.beginPath();
-        ctx.arc(window.innerWidth / 2, window.innerHeight / 2, radius, 6.2, 0, true);
-        ctx.strokeStyle = "#999999";
-        ctx.stroke();
+        for (let i = 0; i < dottedArray.length; i++) {
+            ctx.beginPath();
+            ctx.arc(window.innerWidth / 2, window.innerHeight / 2, radius, dottedArray[i].angleEnd, dottedArray[i].angleStart, true);
+            ctx.strokeStyle = "#999999";
+            ctx.stroke();
+        }
     }
 }
